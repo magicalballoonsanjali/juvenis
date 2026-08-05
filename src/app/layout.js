fd72e5fdd0808ee011e1053e8ffa4e5d59e3eb5f
Your Name <anjalisuthar114@gmail.com>
2026-08-05 16:09:24 +0530

    added meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
 import { Toaster } from 'react-hot-toast'
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +25,21 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-G6XK1QPMFS"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-G6XK1QPMFS');
+          `}
+        </Script>
           <Toaster position="top-right" reverseOrder={false} />
         <Header/>  
        <Navbar/>
